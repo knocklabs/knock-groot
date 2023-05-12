fe4eab3f44939a9a90bd515b62753ee0c942bb52
Thomas <thomas.whyyou@gmail.com>
2023-05-12 12:21:12 -0400

    update

--- src/commands/hello/index.ts
+++ src/commands/hello/index.ts
@@ -2,6 +2,7 @@ import {Command} from '@oclif/core'
 
 export default class Hello extends Command {
   async run(): Promise<void> {
-    this.log("I am Groot 0.0.40")
+
+    this.log(`I am Groot (v${this.config.version})`)
   }
 }
